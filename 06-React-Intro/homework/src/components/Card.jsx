a54c13cf77733e41d168f24ea2148b8f58a1dfc8
Miqueas Ledesma <Mickeas98@gmail.com>
2022-07-26 17:41:08 -0300

    homework06

--- 06-React-Intro/homework/src/components/Card.jsx
+++ 06-React-Intro/homework/src/components/Card.jsx
@@ -2,5 +2,19 @@ import React from 'react';
 
 export default function Card(props) {
   // acá va tu código
-  return <div>Card Component</div>
+
+const tempMax = props.max
+
+const tempMin = props.min
+
+  return <div>
+    <button onClick={props.onClose}>x</button>
+    <p>Max</p>
+    <p>{tempMax}°C</p>
+    <p>Min</p>
+    <p>{tempMin}°C</p>
+    <p>Ciudad</p>
+    <h4>{props.name}</h4>
+  <img src={`http://openweathermap.org/img/wn/${props.img}@2x.png`} alt="Error!" />
+  </div>
 };
