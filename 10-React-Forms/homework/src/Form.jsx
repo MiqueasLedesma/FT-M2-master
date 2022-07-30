f2cc1279bc49730b6aef890afe88a34c6a990a00
Miqueas Ledesma <Mickeas98@gmail.com>
2022-07-30 18:45:37 -0300

    homework10

--- 10-React-Forms/homework/src/Form.jsx
+++ 10-React-Forms/homework/src/Form.jsx
@@ -1,9 +1,48 @@
 import React from 'react';
+import './Validate.jsx'
 
-export default function  Form() {
-  return (
-      <div>
-        Componente Form
-      </div>
-  )
+export default function Form() {
+
+    const [input, setInput] = React.useState({
+        username: '',
+        password: '',
+    });
+
+    const [errors, setErrors] = React.useState({});
+
+    const handleInputChange = function (e) {
+        setInput({
+            ...input,
+            [e.target.name]: e.target.value
+        });
+        setErrors(validate({
+            ...input,
+            [e.target.name]: e.target.value
+        }));
+    }
+
+    return (
+        <form onSubmit={handleInputChange}>
+            <div>
+                <br />
+                <label>Username:</label>
+                <input type="text" name='username' onChange={handleInputChange} value={input.username} />
+                {errors.username && (
+                    <p className="danger">{errors.username}</p>
+                )}
+                <div />
+                <br />
+                <div>
+                    <label>Password:</label>
+                    <input type="password" name='password' onChange={handleInputChange} value={input.password} />
+                </div>
+                <br />
+                <div>
+                    <button>Submit</button>
+                </div>
+            </div>
+        </form>
+    )
 }
+
+
